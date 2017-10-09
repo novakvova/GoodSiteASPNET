1222b63e4519da2a0ac5d82c75de33f44f0aa6f6
novak vova <novakvova@gmail.com>
2017-10-09 18:59:46 +0300

    Add DAL Project

---
+++ DAL/Entities/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(maximumLength: 256)]
+        public string Name { get; set; }
+        public DateTime DateCreate { get; set; }
+        public int Priority { get; set; }
+        virtual public ICollection<City> Cities { get; set; }
+
+    }
+}
---
+++ DAL/Entities/EFContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("TourConnection")
+        {
+            Database.SetInitializer<EFContext>(null);
+        }
+        public EFContext(string connString)
+            :base(connString)
+        {
+            Database.SetInitializer<EFContext>(new DBInitializer());
+        }
+    }
+}
